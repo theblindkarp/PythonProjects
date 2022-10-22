94898eb04a9a05df6f0f5706c63432be2587af7d
theblindkarp <nicknorman0206@gmail.com>
2022-10-22 18:58:53 -0400

    Create love.py

---
+++ love.py
@@ -0,0 +1,17 @@
+import random
+
+keepGoing = True
+while keepGoing:
+    percent = random.randint(1,100)
+    nameNum = int(input("How many people are in the relationship?: "))
+    nameList = []
+    for i in range(nameNum):
+        nameTemp = input(f"What is person {i + 1}'s name: ")
+        nameList.append(nameTemp)
+    lastName = nameList[len(nameList) - 1]
+    nameList.remove(nameList[len(nameList) - 1])
+    nameListJoin = ", ".join(nameList)
+    print("{} and {} have a {}% chance of love".format(nameListJoin, lastName, percent))
+    dummy = input("press 7 to exit: ")
+    if dummy == "7":
+        keepGoing = False
